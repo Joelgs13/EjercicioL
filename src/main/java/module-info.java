1b35f0b38ed2cca95add84ccba0bfb5c95b423f8
Joelgs13 <050213jgonzalez@gmail.com>
2024-11-12 17:44:03 +0100

    fallos mencionados corregidos v2...

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,9 @@ module com.example.ejerciciol {
     requires java.sql;
 
 
-    opens com.example.ejerciciol to javafx.fxml;
-    opens com.example.ejerciciol.Model to javafx.base;
+    opens es.joel.ejerciciol to javafx.fxml;
+    opens es.joel.ejerciciol.Model to javafx.base;
 
-    exports com.example.ejerciciol;
-    exports com.example.ejerciciol.Model;
+    exports es.joel.ejerciciol;
+    exports es.joel.ejerciciol.Model;
 }
